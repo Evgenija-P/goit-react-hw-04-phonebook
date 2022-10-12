b9610e57d629e4ab2cf909c0820b191c8119e264
Evgenija-P <99011054+Evgenija-P@users.noreply.github.com>
2022-10-12 17:11:38 +0300

    app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,77 +9,51 @@ import { AppWrapper, Title, TitleContacts } from './App.styled';
 
 export const App = () => {
   const [contacts, setContacts] = useState(
-    JSON.parse(localStorage.getItem('сontacts')) ?? ''
+    JSON.parse(localStorage.getItem('сontacts')) ?? [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]
   );
-  const [filterTex, setFilterTex] = useState();
-
-  // state = {
-  //   contacts: [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // };
-
-  // function componentDidMount() {
-  // const allContacts = localStorage.getItem('сontacts');
-  // const parsedContacts = JSON.parse(localStorage.getItem('сontacts'));
-
-  // if (parsedContacts) {
-  //   setContacts({ contacts: parsedContacts });
-  // }
-  // }
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   const nextContacts = this.state.contacts;
-  //   const prevContacts = prevState.contacts;
-
-  //   if (nextContacts !== prevContacts) {
-  //     localStorage.setItem('сontacts', JSON.stringify(nextContacts));
-  //   }
-  // }
+  const [filterTex, setFilterTex] = useState('');
 
   useEffect(() => {
     localStorage.setItem('сontacts', JSON.stringify(contacts));
   }, [contacts]);
 
   const addContact = ({ name, number }) => {
-    // const { contacts } = this.state;
-
     const addContact = {
       id: nanoid(5),
       name,
       number,
     };
 
-    // if (
-    //   contacts.find(
-    //     contact => contact.name.toLowerCase() === addContact.name.toLowerCase()
-    //   )
-    // ) {
-    //   return alert(`${addContact.name} is already in contacts.`);
-    // }
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === addContact.name.toLowerCase()
+      )
+    ) {
+      return alert(`${addContact.name} is already in contacts.`);
+    }
 
     setContacts([addContact, ...contacts]);
   };
 
   const deletContact = idContact => {
-    setContacts(({ contacts }) => ({
-      contacts: contacts.filter(contact => contact.id !== idContact),
-    }));
+    setContacts(contacts.filter(contact => contact.id !== idContact));
   };
 
   const filretContacts = e => {
-    setFilterTex({ filterTex: e.currentTarget.value });
+    setFilterTex(e.currentTarget.value);
   };
 
-  // const { filter } = this.state;
-  // const normalized = filter.toLocaleLowerCase();
-  let renderContacts = contacts.filter(contact =>
-    contact.name.toLocaleLowerCase().includes(filterTex.toLocaleLowerCase())
-  );
+  const renderContacts = () => {
+    const normalized = filterTex.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalized)
+    );
+  };
 
   return (
     <AppWrapper>
@@ -88,7 +62,7 @@ export const App = () => {
 
       <TitleContacts>Contacts</TitleContacts>
       <Filter value={filterTex} onFilretContacts={filretContacts} />
-      <ContactList contacts={renderContacts} onDeletContact={deletContact} />
+      <ContactList contacts={renderContacts()} onDeletContact={deletContact} />
     </AppWrapper>
   );
 };
